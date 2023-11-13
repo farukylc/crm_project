1b69ceafcee9f8917b14ed8895a2b98cbf9f0dfe
Faruk Yolcu <ce.farukyolcu@gmail.com>
2023-11-13 17:54:18 +0300

    customer page test

---
+++ project/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project.Models;
+using project.Repositories;
+
+namespace project.Controllers;
+
+public class CustomerController: Controller
+{
+    //Dependency Injection
+    private readonly RepositoryContext _context;
+
+    public CustomerController(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var model = _context.Customers.ToList();
+        return View(model);
+    }
+}
