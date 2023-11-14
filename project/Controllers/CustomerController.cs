d1f1cab97d602cbedb60ba221791c57c0edd03b6
Faruk Yolcu <ce.farukyolcu@gmail.com>
2023-11-14 18:47:01 +0300

    getprofile request

--- project/Controllers/CustomerController.cs
+++ project/Controllers/CustomerController.cs
@@ -8,7 +8,6 @@ namespace project.Controllers;
 public class CustomerController: Controller
 {
     
-    
     public async Task<ActionResult> Index()
     {
         using (HttpClient client = new HttpClient())
@@ -39,22 +38,45 @@ public class CustomerController: Controller
 
         using (HttpClient client = new HttpClient())
         {
-    
             string deleteUrl = apiUrl + id.ToString();
             HttpResponseMessage response = client.DeleteAsync(deleteUrl).Result;
-
-            
             if (response.IsSuccessStatusCode)
             {
-                
                 return RedirectToAction("Index", "Customer");
             }
             else
             {
-                
                 return RedirectToAction("Index", "Customer");
             }
         }
     }
+
+    public ActionResult GetProfile(int id)
+    {
+        string apiUrl = "https://localhost:7222/api/Customer/";
+
+        using (HttpClient client = new HttpClient())
+        {
+            string profileUrl = apiUrl + id.ToString();
+            HttpResponseMessage response = client.GetAsync(profileUrl).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = response.Content.ReadAsStringAsync().Result;
+                var customerProfile = JsonConvert.DeserializeObject<Customer>(responseData);
+
+                
+                return View("Profile", new List<Customer> { customerProfile });
+            }
+            else
+            {
+                return RedirectToAction("Index", "Customer");
+            }
+        }
+    }
+
+    
+
+    
 
 }
